3984125c2f058b0507aa4d3452b59f4d22d96b5e
abdulrafayb <abdulrafay200066@gmail.com>
2025-07-21 02:19:44 +0500

    setting up react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import GlobalStyles from './styles/GlobalStyles';
 import Dashboard from './pages/Dashboard';
@@ -11,9 +13,18 @@ import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './ui/AppLayout';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -31,8 +42,16 @@ function App() {
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
 export default App;
+
+/* the idea behind integrating react query into our application is very similar to context api or redux so first we create a place where the data basically lives and then second we provide that to the application and in the case of react query we set up the cache and the query client using "new QueryClient" coming from tanstack */
+
+/* the query client sets up the cache behind the scenes and we can also pass in a couple of options to override the default options that query client sets up for us so above we specify the default options and then we usually specify options for our queries and above we use staletime which basically means the amount of time that the data in the cache will stay fresh so that it will stay valid until it is refetched again */
+
+/* we provide the query data to the entire application tree basically making the query client parent component of our entire application tree so we use the query client provider component and provide it the actual client */
+
+/* tanstack query also has some excellent dev tools but we just set them up in a different way so in this case it is simply an npm package and not something in the browser and to use them we add them as a first child component of the query provider */
