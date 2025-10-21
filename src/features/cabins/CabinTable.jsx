9a32f2bcaa8775250b5eb49a3cd2b6772af7259a
abdulrafayb <abdulrafay200066@gmail.com>
2025-10-21 20:55:58 +0500

    added sorting in cabins table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -37,6 +37,7 @@ function CabinTable() {
 
   if (isLoading) return <Spinner />;
 
+  // filter
   const filterValue = searchParams.get('discount') || 'all';
   let filteredCabins;
   if (filterValue === 'all') {
@@ -46,6 +47,14 @@ function CabinTable() {
   } else if (filterValue === 'with-discount') {
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
   }
+
+  // sort
+  const sortBy = searchParams.get('sortBy') || 'name-asc';
+  const [field, direction] = sortBy.split('-');
+  const modifier = direction === 'asc' ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
 
   return (
     <Menus>
@@ -60,7 +69,8 @@ function CabinTable() {
         </Table.Header>
         <Table.Body
           // data={cabins}
-          data={filteredCabins}
+          // data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
         />
       </Table>
