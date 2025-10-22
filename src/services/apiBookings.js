a4df65908e9b537a62b0c8e0104f61f119ebcc65
abdulrafayb <abdulrafay200066@gmail.com>
2025-10-22 22:56:09 +0500

    added sorting in bookings table

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -7,10 +7,14 @@ export async function getBookings({ filter, sortBy }) {
     .select('*, cabins(name), guests(fullName, email)');
 
   // filter
-  if (filter !== null)
-    query = query[filter.method || 'eq'](filter.field, filter.value);
+  if (filter) query = query[filter.method || 'eq'](filter.field, filter.value);
 
   // sort
+  if (sortBy) {
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === 'asc',
+    });
+  }
 
   const { data, error } = await query;
 
