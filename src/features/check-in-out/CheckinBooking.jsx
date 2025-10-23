c6dac1e7c4e3f55eb2841e70e9854f36f6406a67
abdulrafayb <abdulrafay200066@gmail.com>
2025-10-23 17:43:09 +0500

    can now add breakfast while checkins in booking

--- src/features/check-in-out/CheckinBooking.jsx
+++ src/features/check-in-out/CheckinBooking.jsx
@@ -12,8 +12,9 @@ import BookingDataBox from '../../features/bookings/BookingDataBox';
 import { formatCurrency } from '../../utils/helpers';
 import { useMoveBack } from '../../hooks/useMoveBack';
 import { useBooking } from '../bookings/useBooking';
+import { useCheckin } from './useCheckin';
+import { useSettings } from '../settings/useSettings';
 import { useEffect, useState } from 'react';
-import { useChecking } from './useCheckin';
 
 const Box = styled.div`
   background-color: var(--color-grey-0);
@@ -24,14 +25,16 @@ const Box = styled.div`
 
 function CheckinBooking() {
   const [confirmPaid, setConfirmPaid] = useState(false);
+  const [addBreakfast, setAddBreakfast] = useState(false);
 
   const { booking, isLoading } = useBooking();
-  const { checkin, isCheckingIn } = useChecking();
+  const { checkin, isCheckingIn } = useCheckin();
+  const { settings, isLoading: isLoadingSettings } = useSettings();
   const moveBack = useMoveBack();
 
   useEffect(() => setConfirmPaid(booking?.isPaid || false), [booking]);
 
-  if (isLoading) return <Spinner />;
+  if (isLoading || isLoadingSettings) return <Spinner />;
 
   const {
     id: bookingId,
@@ -42,9 +45,23 @@ function CheckinBooking() {
     numNights,
   } = booking;
 
+  const optionalBreakfastPrice =
+    settings.breakfastPrice * numNights * numGuests;
+
   function handleCheckin() {
     if (!confirmPaid) return;
-    checkin(bookingId);
+    if (addBreakfast) {
+      checkin({
+        bookingId,
+        breakfast: {
+          hasBreakfast: true,
+          extrasPrice: optionalBreakfastPrice,
+          totalPrice: totalPrice + optionalBreakfastPrice,
+        },
+      });
+    } else {
+      checkin({ bookingId, breakfast: {} });
+    }
   }
 
   return (
@@ -56,6 +73,21 @@ function CheckinBooking() {
 
       <BookingDataBox booking={booking} />
 
+      {!hasBreakfast && (
+        <Box>
+          <Checkbox
+            checked={addBreakfast}
+            onChange={() => {
+              setAddBreakfast((add) => !add);
+              setConfirmPaid(false);
+            }}
+            id='breakfast'
+          >
+            Want to add breakfast for {formatCurrency(optionalBreakfastPrice)}?
+          </Checkbox>
+        </Box>
+      )}
+
       <Box>
         <Checkbox
           checked={confirmPaid}
@@ -64,7 +96,13 @@ function CheckinBooking() {
           id='confirm'
         >
           I confrim that {guests.fullName} has paid the total amount of{' '}
-          {formatCurrency(totalPrice)}
+          {!addBreakfast
+            ? formatCurrency(totalPrice)
+            : `${formatCurrency(
+                totalPrice + optionalBreakfastPrice
+              )} (${formatCurrency(totalPrice)} + ${formatCurrency(
+                optionalBreakfastPrice
+              )})`}
         </Checkbox>
       </Box>
 
--- src/features/check-in-out/useCheckin.js
+++ src/features/check-in-out/useCheckin.js
@@ -3,13 +3,17 @@ import toast from 'react-hot-toast';
 import { updateBooking } from '../../services/apiBookings';
 import { useNavigate } from 'react-router-dom';
 
-export function useChecking() {
+export function useCheckin() {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { mutate: checkin, isLoading: isCheckingIn } = useMutation({
-    mutationFn: (bookingId) =>
-      updateBooking(bookingId, { status: 'checked-in', isPaid: true }),
+    mutationFn: ({ bookingId, breakfast }) =>
+      updateBooking(bookingId, {
+        status: 'checked-in',
+        isPaid: true,
+        ...breakfast,
+      }),
 
     onSuccess: (data) => {
       toast.success(`Booking #${data.id} successfully checked in`);
@@ -26,3 +30,5 @@ export function useChecking() {
 /* the onsuccess function above actually receives some data which is the returned data from the function passed in mutation function */
 
 /* active true will invalidate all the queries that are currently active on the page which is a bit easier because then we don't have to remember any query keys */
+
+/* so the mutation function can only receive on argument so instead of just passing it the bookingid we will pass in an object */
