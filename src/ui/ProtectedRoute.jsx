994d84844e60b8d5d219fded2406c6150e9d590c
abdulrafayb <abdulrafay200066@gmail.com>
2025-10-25 20:38:10 +0500

    added protected routes

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+import Spinner from './Spinner';
+
+import { useUser } from '../features/authentication/useUser';
+import { useEffect } from 'react';
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+
+  // load the authenticated user
+  const { isLoading, isAuthenticated } = useUser();
+
+  // if there is no authenticated user, redirect to the /login
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate('/login');
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  // while loading, show a spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />;
+      </FullPage>
+    );
+
+  // if there is a user, render the app
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
