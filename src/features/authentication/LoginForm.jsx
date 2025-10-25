55d4bc415644782ac877409117dff55a1686fd0a
abdulrafayb <abdulrafay200066@gmail.com>
2025-10-25 20:07:25 +0500

    added login page with login functionality

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,38 +1,51 @@
-import { useState } from "react";
-import Button from "../../ui/Button";
-import Form from "../../ui/Form";
-import FormRow from "../../ui/FormRow";
-import Input from "../../ui/Input";
+import Button from '../../ui/Button';
+import Form from '../../ui/Form';
+import FormRow from '../../ui/FormRow';
+import Input from '../../ui/Input';
+import SpinnerMini from '../../ui/SpinnerMini';
+
+import { useState } from 'react';
+import { useLogin } from './useLogin';
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('rafay@example.com');
+  const [password, setPassword] = useState('pass1234');
+  const { login, isLoading } = useLogin();
 
-  function handleSubmit() {}
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+
+    login({ email, password });
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
-      <FormRow label="Email address" orientation="vertical">
+      <FormRow label='Email address' orientation='vertical'>
         <Input
-          type="email"
-          id="email"
+          type='email'
+          id='email'
           // This makes this form better for password managers
-          autoComplete="username"
+          autoComplete='username'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoading}
         />
       </FormRow>
-      <FormRow label="Password" orientation="vertical">
+      <FormRow label='Password' orientation='vertical'>
         <Input
-          type="password"
-          id="password"
-          autoComplete="current-password"
+          type='password'
+          id='password'
+          autoComplete='current-password'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isLoading}
         />
       </FormRow>
-      <FormRow orientation="vertical">
-        <Button size="large">Login</Button>
+      <FormRow orientation='vertical'>
+        <Button size='large' disabled={isLoading}>
+          {!isLoading ? 'Log in' : <SpinnerMini />}
+        </Button>
       </FormRow>
     </Form>
   );
