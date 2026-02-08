9a35f5cbb131fa989c149bd60a87a356b6f4904b
abdulrafayb <abdulrafay200066@gmail.com>
2026-02-08 21:51:05 +0500

    added four cards on dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import { useRecentBookings } from './useRecentBookings';
 import Spinner from '../../ui/Spinner';
 import { useRecentStays } from './useRecentStays';
+import Stats from './Stats';
+import { useCabins } from '../cabins/useCabins';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -12,13 +14,28 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
   const { bookings, isLoading: isLoading1 } = useRecentBookings();
-  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
 
-  if (isLoading1 || isLoading2) return <Spinner />;
+  const {
+    stays,
+    confirmedStays,
+    numDays,
+    isLoading: isLoading2,
+  } = useRecentStays();
 
-  console.log(bookings);
+  const { cabins, isLoading: isLoading3 } = useCabins();
 
-  return <StyledDashboardLayout></StyledDashboardLayout>;
+  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins?.length}
+      />
+    </StyledDashboardLayout>
+  );
 }
 
 export default DashboardLayout;
